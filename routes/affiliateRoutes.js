a0d4e0028f82f74b81e548051317cbd893d14155
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-08-22 11:21:50 +0530

    patch updated

--- routes/affiliateRoutes.js
+++ routes/affiliateRoutes.js
@@ -18,6 +18,7 @@ router.post('/affiliateLogin', loginAffiliate);
 router.get('/getAllAffiliate', getAllAffiliates);
 router.get('/getOneAffiliate/:id', getAffiliateById);
 router.put('/updateAffiliate/:id', updateAffiliate);
+router.patch('/updatePostbackUrl/:id', updatePostBackUrl);
 router.delete('/deleteAffiliate/:id', deleteAffiliate);
 
 
